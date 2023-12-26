af076390312f5073057186ea32ce5ce91517cbbc
BainaryTD <121595278+BainaryTD@users.noreply.github.com>
2023-12-26 18:29:51 +0700

    Added vuetify

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,18 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 const app = createApp(App)
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
 
 app.use(createPinia())
 app.use(router)
 
-app.mount('#app')
+app.use(vuetify).mount('#app')
